89e7f66c4298c543725b2ae7ed00945f2ef81fa7
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-02-21 13:44:33 -0500

    Add Ticket representation with JSON marshaling support

---
+++ backend/types.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type Ticket struct {
+	ID                    int       `json:"id"`
+	Description           string    `json:"description"`
+	DateCreated           time.Time `json:"date_created"`
+	LastModified          time.Time `json:"last_modified"`
+	CustomerName          string    `json:"customer_name"`
+	CustomerEmail         string    `json:"customer_email"`
+	Status                string    `json:"status"`
+	EmployeeName          string    `json:"employee_name"`
+	ResolutionDescription string    `json:"resolution_description"`
+}
