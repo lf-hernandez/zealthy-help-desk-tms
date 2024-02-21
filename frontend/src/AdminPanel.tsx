797c3aebbe5c03762816bb9c85cf74dff98b1e5f
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-02-21 17:57:39 -0500

    Wire up update status call

--- frontend/src/AdminPanel.tsx
+++ frontend/src/AdminPanel.tsx
@@ -41,7 +41,7 @@ export const AdminPanel = () => {
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-    const [tickets, setTickets] = React.useState<Array<Ticket>>([]);
+    const [tickets, setTickets] = React.useState<Array<Ticket>>();
     const [selectedTicket, setSelectedTicket] = React.useState<Ticket>();
 
     const [status, setStatus] = React.useState('');
@@ -50,28 +50,50 @@ export const AdminPanel = () => {
 
     const [modalOpen, setModalOpen] = React.useState(false);
 
-    React.useEffect(() => {
-        const getAllTickets = async () => {
-            try {
-                const response = await fetch(BASE_URL, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
+    const getAllTickets = async () => {
+        try {
+            const response = await fetch(BASE_URL, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-                const data = await response.json();
-                setTickets(data);
-            } catch (e) {
-                alert('Failed to load tickets');
-                console.error(e);
+            const data = await response.json();
+            setTickets(data);
+        } catch (e) {
+            alert('Failed to load tickets');
+            console.error(e);
+        }
+    };
+
+    const updateTicketStatus = async (id: string) => {
+        try {
+            const response = await fetch(`${BASE_URL}/${id}/status`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    status,
+                    employee_name: assignee,
+                    resolution_description: resolutionDescription,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
             }
-        };
+        } catch (e) {
+            alert('Failed to update ticket');
+            console.error(e);
+        }
+        await getAllTickets();
+    };
 
+    React.useEffect(() => {
         getAllTickets();
     }, []);
-
-    console.log('data: ', tickets);
 
     const handleRowClick = (ticket: Ticket) => {
         setSelectedTicket(ticket);
@@ -83,12 +105,18 @@ export const AdminPanel = () => {
     const handleStatusChange = (event: SelectChangeEvent) =>
         setStatus(event.target.value as string);
     const handleClose = () => setModalOpen(false);
-    const handleUpdate = () => {
+    const onUpdate = async () => {
+        if (!selectedTicket) {
+            setModalOpen(false);
+            return;
+        }
+        await updateTicketStatus(selectedTicket.id);
         setAssignee('');
         setStatus('');
         setResolutionDescription('');
         setModalOpen(false);
     };
+
     return (
         <>
             <Typography variant="h2" gutterBottom>
@@ -96,99 +124,108 @@ export const AdminPanel = () => {
             </Typography>
             <hr />
             <br />
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Ticket ID</TableCell>
-                            <TableCell align="left">Description</TableCell>
-                            <TableCell align="left">Assignee</TableCell>
-                            <TableCell align="left">Status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {tickets.map((ticket) => (
-                            <TableRow
-                                key={ticket.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                onClick={() => handleRowClick(ticket)}
-                                style={{ cursor: 'pointer' }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {ticket.id}
-                                </TableCell>
-                                <TableCell align="left">{ticket.description}</TableCell>
-                                <TableCell align="left">{ticket.employee_name}</TableCell>
-                                <TableCell align="left">{ticket.status}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            {tickets && tickets?.length > 0 ? (
+                <>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Ticket ID</TableCell>
+                                    <TableCell align="left">Description</TableCell>
+                                    <TableCell align="left">Assignee</TableCell>
+                                    <TableCell align="left">Status</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {tickets.map((ticket) => (
+                                    <TableRow
+                                        key={ticket.id}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        onClick={() => handleRowClick(ticket)}
+                                        style={{ cursor: 'pointer' }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {ticket.id}
+                                        </TableCell>
+                                        <TableCell align="left">{ticket.description}</TableCell>
+                                        <TableCell align="left">{ticket.employee_name}</TableCell>
+                                        <TableCell align="left">{ticket.status}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
 
-            <Dialog open={modalOpen} onClose={handleClose} fullScreen={fullScreen}>
-                <DialogTitle>{'Ticket Details'}</DialogTitle>
+                    <Dialog open={modalOpen} onClose={handleClose} fullScreen={fullScreen}>
+                        <DialogTitle>{'Ticket Details'}</DialogTitle>
 
-                <DialogContent>
-                    <div>
-                        <Typography sx={{ mt: 2 }}>
-                            {selectedTicket && `ID: ${selectedTicket.id}`}
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            {selectedTicket && `Created on: ${selectedTicket.date_created}`}
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            {selectedTicket && `Description: ${selectedTicket.description}`}
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            {selectedTicket && `Customer Name: ${selectedTicket.customer_name}`}
-                        </Typography>
-                        <Typography sx={{ mt: 2 }}>
-                            {selectedTicket && `Customer Email: ${selectedTicket.customer_email}`}
-                        </Typography>
-                        <TextField
-                            fullWidth
-                            label="Assignee"
-                            value={assignee}
-                            variant="outlined"
-                            margin="normal"
-                            onChange={(e) => setAssignee(e.target.value)}
-                        />
-                        <Typography sx={{ my: 2 }}>
-                            {selectedTicket && `Last modified: ${selectedTicket.last_modified}`}
-                        </Typography>
+                        <DialogContent>
+                            <div>
+                                <Typography sx={{ mt: 2 }}>
+                                    {selectedTicket && `ID: ${selectedTicket.id}`}
+                                </Typography>
+                                <Typography sx={{ mt: 2 }}>
+                                    {selectedTicket && `Created on: ${selectedTicket.date_created}`}
+                                </Typography>
+                                <Typography sx={{ mt: 2 }}>
+                                    {selectedTicket && `Description: ${selectedTicket.description}`}
+                                </Typography>
+                                <Typography sx={{ mt: 2 }}>
+                                    {selectedTicket &&
+                                        `Customer Name: ${selectedTicket.customer_name}`}
+                                </Typography>
+                                <Typography sx={{ mt: 2 }}>
+                                    {selectedTicket &&
+                                        `Customer Email: ${selectedTicket.customer_email}`}
+                                </Typography>
+                                <TextField
+                                    fullWidth
+                                    label="Assignee"
+                                    value={assignee}
+                                    variant="outlined"
+                                    margin="normal"
+                                    onChange={(e) => setAssignee(e.target.value)}
+                                />
+                                <Typography sx={{ my: 2 }}>
+                                    {selectedTicket &&
+                                        `Last modified: ${selectedTicket.last_modified}`}
+                                </Typography>
 
-                        <FormControl fullWidth>
-                            <InputLabel id="demo-simple-select-label">Status</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-label"
-                                value={status}
-                                label="Status"
-                                onChange={handleStatusChange}
-                            >
-                                <MenuItem value={'new'}>New</MenuItem>
-                                <MenuItem value={'in progress'}>In Progress</MenuItem>
-                                <MenuItem value={'resolved'}>Resolved</MenuItem>
-                            </Select>
-                        </FormControl>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        value={status}
+                                        label="Status"
+                                        onChange={handleStatusChange}
+                                    >
+                                        <MenuItem value={'new'}>New</MenuItem>
+                                        <MenuItem value={'in progress'}>In Progress</MenuItem>
+                                        <MenuItem value={'resolved'}>Resolved</MenuItem>
+                                    </Select>
+                                </FormControl>
 
-                        <TextField
-                            fullWidth
-                            label="Resolution comment"
-                            multiline
-                            rows={4}
-                            variant="outlined"
-                            margin="normal"
-                            value={resolutionDescription}
-                            onChange={(e) => setResolutionDescription(e.target.value)}
-                        />
-                    </div>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleUpdate}>Update</Button>
-                </DialogActions>
-            </Dialog>
+                                <TextField
+                                    fullWidth
+                                    label="Resolution comment"
+                                    multiline
+                                    rows={4}
+                                    variant="outlined"
+                                    margin="normal"
+                                    value={resolutionDescription}
+                                    onChange={(e) => setResolutionDescription(e.target.value)}
+                                />
+                            </div>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button onClick={onUpdate}>Update</Button>
+                        </DialogActions>
+                    </Dialog>
+                </>
+            ) : (
+                'No tickets yet'
+            )}
         </>
     );
 };
