53b2afe3e8192ac62ab8d8280501f4ebe0c870a7
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-02-21 16:14:42 -0500

    Implement remainding handlers

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -57,13 +58,223 @@ func (server *APIServer) handleNewTicket(response http.ResponseWriter, request *
 }
 
 func (server *APIServer) handleUpdateTicketStatus(response http.ResponseWriter, request *http.Request) error {
-	return nil
+	if request.Method != "PUT" {
+		http.Error(response, "Only PUT method is allowed", http.StatusMethodNotAllowed)
+		return nil
+	}
+
+	id := request.PathValue("id")
+
+	var (
+		ticketToUpdate        Ticket
+		employeeName          sql.NullString
+		resolutionDescription sql.NullString
+	)
+	err := db.QueryRow(`
+        SELECT 
+			id, 
+			description,
+			created_at,
+			modified_at, 
+			customer_name, 
+			customer_email, 
+			status,
+			employee_name,
+			resolution_description
+        FROM ticket 
+        WHERE id = $1`, id).
+		Scan(&ticketToUpdate.ID,
+			&ticketToUpdate.Description,
+			&ticketToUpdate.DateCreated,
+			&ticketToUpdate.LastModified,
+			&ticketToUpdate.CustomerEmail,
+			&ticketToUpdate.CustomerName,
+			&ticketToUpdate.Status,
+			&employeeName,
+			&resolutionDescription,
+		)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(response, "Ticket not found", http.StatusNotFound)
+			return nil
+		}
+		return err
+	}
+
+	var ticketInput struct {
+		Status                string `json:"status"`
+		EmployeeName          string `json:"employee_name"`
+		ResolutionDescription string `json:"resolution_description"`
+	}
+	err = json.NewDecoder(request.Body).Decode(&ticketInput)
+	if err != nil {
+		http.Error(response, "Bad request", http.StatusBadRequest)
+	}
+
+	_, err = db.Exec(`
+		UPDATE ticket 
+		SET status = $1, 
+			employee_name = $2,
+			resolution_description = $3
+		WHERE id = $4`,
+		ticketInput.Status,
+		ticketInput.EmployeeName,
+		ticketInput.ResolutionDescription,
+		id)
+	if err != nil {
+		log.Printf("[ERROR] failed to update ticket: %v", err)
+		http.Error(response, "Internal server error", http.StatusInternalServerError)
+		return nil
+	}
+
+	var updatedTicket Ticket
+	err = db.QueryRow(`
+        SELECT 
+			id, 
+			description,
+			created_at,
+			modified_at, 
+			customer_name, 
+			customer_email, 
+			status,
+			employee_name,
+			resolution_description
+        FROM ticket 
+        WHERE id = $1`, id).
+		Scan(&updatedTicket.ID,
+			&updatedTicket.Description,
+			&updatedTicket.DateCreated,
+			&updatedTicket.LastModified,
+			&updatedTicket.CustomerEmail,
+			&updatedTicket.CustomerName,
+			&updatedTicket.Status,
+			&updatedTicket.EmployeeName,
+			&updatedTicket.ResolutionDescription,
+		)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(response, "Ticket not found", http.StatusNotFound)
+			return nil
+		}
+		return err
+	}
+
+	return writeJSON(response, http.StatusOK, updatedTicket)
 }
 
 func (server *APIServer) handleTicketById(response http.ResponseWriter, request *http.Request) error {
-	return nil
+	if request.Method != "GET" {
+		http.Error(response, "Only GET method is allowed", http.StatusMethodNotAllowed)
+		return nil
+	}
+
+	id := request.PathValue("id")
+
+	var (
+		ticket                Ticket
+		employeeName          sql.NullString
+		resolutionDescription sql.NullString
+	)
+	err := db.QueryRow(`SELECT 
+						id, 
+						description, 
+						created_at, 
+						modified_at, 
+						customer_name, 
+						customer_email, 
+						status,
+						employee_name,
+						resolution_description
+					FROM ticket 
+					WHERE id = $1`, id).
+		Scan(&ticket.ID,
+			&ticket.Description,
+			&ticket.DateCreated,
+			&ticket.LastModified,
+			&ticket.CustomerName,
+			&ticket.CustomerName,
+			&ticket.Status,
+			&employeeName,
+			&resolutionDescription,
+		)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(response, "Ticket not found", http.StatusNotFound)
+			return nil
+		}
+		return err
+	}
+
+	if employeeName.Valid {
+		ticket.EmployeeName = employeeName.String
+	}
+	if resolutionDescription.Valid {
+		ticket.ResolutionDescription = resolutionDescription.String
+	}
+	return writeJSON(response, http.StatusOK, ticket)
 }
 
 func (server *APIServer) handleAllTickets(response http.ResponseWriter, request *http.Request) error {
-	return nil
+	if request.Method != "GET" {
+		http.Error(response, "Only GET method is allowed", http.StatusMethodNotAllowed)
+		return nil
+	}
+
+	rows, err := db.Query(`SELECT 
+							id, 
+							description, 
+							created_at, 
+							modified_at, 
+							customer_name, 
+							customer_email, 
+							status,
+							employee_name,
+							resolution_description
+						FROM ticket`)
+	if err != nil {
+		log.Printf("[ERROR] failed to execute query: %v", err)
+		http.Error(response, "Internal server error", http.StatusInternalServerError)
+		return nil
+	}
+	defer rows.Close()
+
+	var tickets []Ticket
+	for rows.Next() {
+		var (
+			ticket                Ticket
+			employeeName          sql.NullString
+			resolutionDescription sql.NullString
+		)
+		err := rows.Scan(
+			&ticket.ID,
+			&ticket.Description,
+			&ticket.DateCreated,
+			&ticket.LastModified,
+			&ticket.CustomerName,
+			&ticket.CustomerEmail,
+			&ticket.Status,
+			&employeeName,
+			&resolutionDescription,
+		)
+		if err != nil {
+			log.Printf("[ERROR] failed to scan row: %v", err)
+			continue
+		}
+		if employeeName.Valid {
+			ticket.EmployeeName = employeeName.String
+		}
+		if resolutionDescription.Valid {
+			ticket.ResolutionDescription = resolutionDescription.String
+		}
+
+		tickets = append(tickets, ticket)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("[ERROR] error iterating rows: %v", err)
+		http.Error(response, "Internal server error", http.StatusInternalServerError)
+		return nil
+	}
+
+	return writeJSON(response, http.StatusOK, tickets)
 }
