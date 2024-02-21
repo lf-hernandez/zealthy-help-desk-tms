3a8c0e91b04f8b5bc678745f6df520e9295abc2d
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-02-21 15:46:22 -0500

    Add page navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,71 +1,45 @@
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
+import { Box, Container, Drawer, List, ListItem, ListItemText } from '@mui/material';
 import * as React from 'react';
-const BASE_URL = `${import.meta.env.VITE_API_URL}/tickets/`;
+
+import { AdminPanel } from './AdminPanel';
+import { SubmitTicket } from './SubmitTicket';
 
 function App() {
-    const [customerName, setCustomerName] = React.useState('');
-    const [customerEmail, setCustomerEmail] = React.useState('');
-    const [description, setDescription] = React.useState('');
-
-    const handleSubmit = async () => {
-        try {
-            await fetch(BASE_URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    customer_name: customerName,
-                    customer_email: customerEmail,
-                    description: description,
-                }),
-            });
-        } catch (e) {
-            alert('Failed to submit ticket');
-            console.error(e);
-        }
+    const [showAdminPanel, setShowAdminPanel] = React.useState(false);
+    const drawerWidth = 240;
+    const activeItemBackground = {
+        backgroundColor: '#f4f4f4',
     };
-
     return (
-        <Container maxWidth="md">
-            <Typography variant="h2" gutterBottom>
-                Submit Support Ticket
-            </Typography>
-            <hr />
-            <form onSubmit={handleSubmit}>
-                <Box my={4}>
-                    <TextField
-                        fullWidth
-                        label="Name"
-                        variant="outlined"
-                        margin="normal"
-                        onChange={(e) => setCustomerName(e.target.value)}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        margin="normal"
-                        onChange={(e) => setCustomerEmail(e.target.value)}
-                    />
-                    <TextField
-                        fullWidth
-                        label="Description"
-                        multiline
-                        rows={4}
-                        variant="outlined"
-                        margin="normal"
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </Box>
+        <Box sx={{ display: 'flex' }}>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+            >
+                <List>
+                    <ListItem
+                        onClick={() => setShowAdminPanel(false)}
+                        sx={!showAdminPanel ? activeItemBackground : null}
+                    >
+                        <ListItemText primary="Submit Ticket" />
+                    </ListItem>
+                    <ListItem
+                        onClick={() => setShowAdminPanel(true)}
+                        sx={showAdminPanel ? activeItemBackground : null}
+                    >
+                        <ListItemText primary="Admin Panel" />
+                    </ListItem>
+                </List>
+            </Drawer>
 
-                <Box display="flex" justifyContent="flex-end" mt={2}>
-                    <Button type="submit" variant="contained" color="primary">
-                        Submit
-                    </Button>
-                </Box>
-            </form>
-        </Container>
+            <Container maxWidth="md">
+                {showAdminPanel ? <AdminPanel /> : <SubmitTicket />}
+            </Container>
+        </Box>
     );
 }
 
---
+++ frontend/src/SubmitTicket.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
+import * as React from 'react';
+
+const BASE_URL = `${import.meta.env.VITE_API_URL}/tickets/`;
+
+export const SubmitTicket = () => {
+    const [customerName, setCustomerName] = React.useState('');
+    const [customerEmail, setCustomerEmail] = React.useState('');
+    const [description, setDescription] = React.useState('');
+
+    const handleSubmit = async () => {
+        try {
+            await fetch(BASE_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    customer_name: customerName,
+                    customer_email: customerEmail,
+                    description: description,
+                }),
+            });
+        } catch (e) {
+            alert('Failed to submit ticket');
+            console.error(e);
+        }
+    };
+
+    return (
+        <>
+            <Typography variant="h2" gutterBottom>
+                Submit Support Ticket
+            </Typography>
+            <hr />
+            <form onSubmit={handleSubmit}>
+                <Box my={4}>
+                    <TextField
+                        fullWidth
+                        label="Name"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(e) => setCustomerName(e.target.value)}
+                    />
+                    <TextField
+                        fullWidth
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(e) => setCustomerEmail(e.target.value)}
+                    />
+                    <TextField
+                        fullWidth
+                        label="Description"
+                        multiline
+                        rows={4}
+                        variant="outlined"
+                        margin="normal"
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </Box>
+
+                <Box display="flex" justifyContent="flex-end" mt={2}>
+                    <Button type="submit" variant="contained" color="primary">
+                        Submit
+                    </Button>
+                </Box>
+            </form>
+        </>
+    );
+};
