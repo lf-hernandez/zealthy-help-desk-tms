e43ff2d31603f09da433ecf68a53167010bdc8d3
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2024-02-21 20:26:31 -0500

    Make table wider

--- frontend/src/AdminPanel.tsx
+++ frontend/src/AdminPanel.tsx
@@ -127,7 +127,7 @@ export const AdminPanel = () => {
             {tickets && tickets?.length > 0 ? (
                 <>
                     <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <Table sx={{ minWidth: 750 }} aria-label="simple table">
                             <TableHead>
                                 <TableRow>
                                     <TableCell>Ticket ID</TableCell>
